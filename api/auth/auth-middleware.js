1a7252944abf38aa927c49962418a4ae56e2c933
mrn720 <matt.ryan.norris@gmail.com>
2022-01-15 13:49:25 -0600

    MVP

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,6 @@
+const Users = require('../users/users-model')
+const db = require('../../data/db-config')
+
 /*
   If the user does not have a session saved in the server
 
@@ -6,8 +9,12 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+const restricted = (req, res, next) => {
+  if(req.session && req.session.username) {
+    next()
+  } else {
+    res.status(401).json("You shall not pass!")
+  }
 }
 
 /*
@@ -18,8 +25,17 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
+const checkUsernameFree = async (req, res, next) => {
+  try {
+    const user = await Users.findBy({username: req.body.username})
+    if(!user.length) {
+      next()
+    } else {
+      res.status(422).json("Username taken")
+    }
+  } catch(err) {
+    res.status(500).json(`Message: ${err}`)
+  }
 }
 
 /*
@@ -30,8 +46,18 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+const checkUsernameExists = async (req, res, next) => {
+  try {
+    const user = await Users.findBy({username: req.body.username})
+    if (user.length) {
+      req.userData = user[0]
+      next()
+    } else {
+      res.status(401).json("Invalid credentials")
+    }
+  } catch(err) {
+    res.status(500).json(`Message: ${err}`)
+  }
 }
 
 /*
@@ -42,8 +68,23 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+const checkPasswordLength = async (req, res, next) => {
+  try {
+    if(!req.body.password || req.body.password.length <= 3) {
+      res.status(422).json("Password must be longer than 3 chars")
+    } else {
+      next()
+    }
+  } catch(err) {
+    res.status(500).json(`Message: ${err}`)
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength
+}
